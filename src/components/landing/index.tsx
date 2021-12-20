0922c91510bb64ea3f94074f0ee8dab085f9072b
Juliet Nwagwu Ume-Ezeoke <julietnwagwuume-ezeoke@Juliets-MacBook-Pro-2.local>
2021-12-20 04:25:14 +0100

    fix ts issue!! :)

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Title from './title';
 import Subtitle from './subtitle';
 import Photos from './photos';
-import {section} from "./index.module.scss"
+import styles from "./index.module.scss"
 
 const LandingIndex = () => {
     return (
-        <section className={section}>
+        <section className={styles.section}>
             <Title></Title>
             <Subtitle></Subtitle>
             <Photos></Photos>
--- src/components/landing/title.tsx
+++ src/components/landing/title.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {h1, h2}  from  './title.module.scss' 
+import styles  from  './title.module.scss' 
 
 
 
 const Title = () => {
     return (
         <div>
-        <h1  className={h1}>
+        <h1  className={styles.h1}>
            DariaIzbash 
         </h1>
         </div> 
